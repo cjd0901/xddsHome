eff31a7cbd447f27301450b558690a962e64d410
xdd <80866702@qq.com>
2020-06-18 21:30:04 +0800

    06-18

---
+++ src/components/HelloWord/index.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './index.scss'
+
+export default class HelloWorld extends Component {
+    render() {
+        return (
+            <div className="cont">
+                <span>H</span>
+                <span>e</span>
+                <span>l</span>
+                <span>l</span>
+                <span>o</span>
+                <span> </span>
+                <span>W</span>
+                <span>o</span>
+                <span>r</span>
+                <span>l</span>
+                <span>d</span>
+            </div>
+        )
+    }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import HelloWorld from '../components/HelloWord'
+
+export default class Root extends Component {
+    render() {
+        return (
+            <div>
+                <HelloWorld></HelloWorld>
+            </div>
+        )
+    }
+}
